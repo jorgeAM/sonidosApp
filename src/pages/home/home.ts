70c2060c46a84a1296121420d3cba0f46ee7229c
Jorge <jorge.alfmur@gmail.com>
2017-06-03 00:32:29 -0500

    mostramos lista de animales con su respectivo icono, usamos una interface

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+//imṕortamos data de animales
+import { ANIMALES } from '../../data/data.animales';
+//exportamos interface
+import { Animal } from '../../interfaces/animal.interface';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	animales:Animal[] = [];
 
   constructor(public navCtrl: NavController) {
-
+  	//crea clon de ANIMALEs
+  	this.animales = ANIMALES.slice(0);
   }
 
 }
